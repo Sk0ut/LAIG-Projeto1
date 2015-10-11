20f2b985747e4040a23ba39419e413cb7dbebb0c
Sk0ut <poke_flavio@hotmail.com>
2015-10-11 19:30:21 +0100

    Sphere drawing completely

--- reader/primitives/MySphere.js
+++ reader/primitives/MySphere.js
@@ -22,45 +22,22 @@
  	this.normals = [];
  	this.texCoords = [];
 
-	var angle = 2*Math.PI / this.slices;
-	var angle_height = (Math.PI / 2)/this.stacks;
-	var stepS = 0;
-	var stepT = 0;
-	// topo
-	for (var stack = 0; stack <= this.stacks; stack++){
-		for (var slice = 0; slice <= this.slices; slice++){
-			this.vertices.push(this.radius * Math.cos(slice * angle) * Math.cos(stack * angle_height), this.radius * Math.sin(slice * angle) * Math.cos(stack * angle_height), this.radius * Math.sin(stack * angle_height));
-			this.normals.push(Math.cos(slice * angle) * Math.cos(stack * angle_height), Math.sin(slice * angle) * Math.cos(stack * angle_height), Math.sin(stack * angle_height));
-			this.texCoords.push(stepS, stepT);
-			stepS+=1/this.slices;
+ 	var dTheta = Math.PI / this.stacks;
+ 	var dPhi = 2 * Math.PI / this.slices;
+
+	for (var stack = 0; stack <= this.stacks; ++stack) {
+		for (var slice = 0; slice <= this.slices; ++slice) {
+			this.vertices.push(this.radius * Math.sin(stack * dTheta) * Math.cos(slice * dPhi), this.radius * Math.sin(stack * dTheta) * Math.sin(slice * dPhi), this.radius * Math.cos(stack * dTheta));
+			this.normals.push(Math.sin(stack * dTheta) * Math.cos(slice * dPhi), Math.sin(stack * dTheta) * Math.sin(slice * dPhi), Math.cos(stack * dTheta));
+			this.texCoords.push(slice/this.slices, 1-stack/this.stacks);
 		}
-		stepS = 0;
-		stepT+= 1/this.stacks;
 	}
 
-	this.vertices.push(0, 0, 1);
-	this.normals.push(0, 0, 1);
-	
-	var n = this.slices + 1;
-	for (var stack = 0; stack < this.stacks - 1; stack++){
-		for (var slice = 0; slice < this.slices; slice++){
-			this.indices.push(stack * n + slice, stack * n + ((slice + 1) % n), (stack + 1) * n + ((slice + 1) % n));
-			this.indices.push(stack * n + slice, (stack + 1) * n + ((slice + 1) % n), (stack + 1) * n + slice);
+	for (var stack = 0; stack < this.stacks; ++stack) {
+		for (var slice = 0; slice < this.slices; ++slice) {
+			this.indices.push(stack * (this.slices + 1) + slice, (stack + 1) * (this.slices + 1) + slice, (stack + 1) * (this.slices + 1) + slice + 1);
+			this.indices.push(stack * (this.slices + 1) + slice, (stack + 1) * (this.slices + 1) + slice + 1, stack * (this.slices + 1) + slice + 1);
 		}
-	}
-	
-	this.vertices.push(0, 0, 0);
-	// base
-	for (var slice = -1; slice < this.slices; slice++){
-		this.vertices.push(Math.cos(slice * angle), Math.sin(slice * angle), 0);
-		this.normals.push(0, 0, -1);
-	}
-
-	for (var slice = 0; slice < this.slices; slice++){
-		if (slice + 1 >= this.slices){
-			this.indices.push(this.stacks * this.slices + 2 + slice, this.stacks * this.slices + 1, this.stacks * this.slices + 2);
-		}
-		else this.indices.push(this.stacks * this.slices + 2 + slice, this.stacks * this.slices + 1, this.stacks * this.slices + 3 + slice);
 	}
 
  	this.primitiveType = this.scene.gl.TRIANGLES;
